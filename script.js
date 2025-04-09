dc614efcc70dfd435f0ae57f905d8ae5ab292b5e
alfonskyy <alfonskyhlg@hotmail.com>
2025-04-09 15:23:41 -0600

    modified flex basis size to avoid flodding

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
 const gridContainer = document.querySelector('.grid-container');
-const resizeBtn = document.getElementById('resizeBtn'); 
 let gridSize = 16;
 let totalSquares = gridSize * gridSize;
-const gridBorderThickness = 3; 
+const gridBorderThickness = 3;
 
 function createGrid() {
     gridContainer.innerHTML = '';
 
-    
     const availableWidth = gridContainer.offsetWidth - (gridBorderThickness * 2);
     const availableHeight = gridContainer.offsetHeight - (gridBorderThickness * 2);
 
-    
     const squareWidth = (availableWidth / gridSize);
     const squareHeight = (availableHeight / gridSize);
+    const squareFlexBasis = `${(100 / gridSize)}%`; 
 
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
         square.style.width = `${squareWidth}px`;
         square.style.height = `${squareHeight}px`;
+        square.style.flexBasis = squareFlexBasis; 
         gridContainer.appendChild(square);
     }
-    
 }
 
 createGrid();
 const squares = document.querySelectorAll('.grid-square');
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red'; 
+        square.style.backgroundColor = 'red';
     });
 });
 
@@ -42,7 +40,7 @@ resizeBtn.addEventListener('click', () => {
         if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
             gridSize = newSize;
             totalSquares = gridSize * gridSize;
-            createGrid(); 
+            createGrid();
             const squares = document.querySelectorAll('.grid-square');
             squares.forEach(square => {
                 square.addEventListener('mouseover', () => {
