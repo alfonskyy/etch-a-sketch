8c180fec11ee4404d1980996ddddf505bae38880
alfonskyy <alfonskyhlg@hotmail.com>
2025-04-09 15:00:11 -0600

    Modified the whole css and js to be able to use flexbox and define the grids Also implemented hover effect

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 const gridContainer = document.querySelector('.grid-container');
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
+let gridSize = 16;
+let totalSquares = gridSize * gridSize;
+const gridBorderThickness = 3; 
 
 function createGrid() {
+    gridContainer.innerHTML = '';
+
+    
+    const availableWidth = gridContainer.offsetWidth - (gridBorderThickness * 2);
+    const availableHeight = gridContainer.offsetHeight - (gridBorderThickness * 2);
+
+    
+    const squareWidth = (availableWidth / gridSize);
+    const squareHeight = (availableHeight / gridSize);
+
     for (let i = 0; i < totalSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('grid-square');
+        square.style.width = `${squareWidth}px`;
+        square.style.height = `${squareHeight}px`;
         gridContainer.appendChild(square);
     }
+    
 }
 
 createGrid();
+const squares = document.querySelectorAll('.grid-square');
+squares.forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'red'; 
+    });
+});
