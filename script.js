c7519d9e8c098961362d54279705ba7be203fa94
alfonskyy <alfonskyhlg@hotmail.com>
2025-04-09 15:19:39 -0600

    implemented button to resize the grid

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
+const resizeBtn = document.getElementById('resizeBtn'); 
 let gridSize = 16;
 let totalSquares = gridSize * gridSize;
 const gridBorderThickness = 3; 
@@ -30,4 +31,26 @@ squares.forEach(square => {
     square.addEventListener('mouseover', () => {
         square.style.backgroundColor = 'red'; 
     });
+});
+
+resizeBtn.addEventListener('click', () => {
+    const newSizeStr = prompt("Enter the number of squares per side for the new grid (100 at max):");
+
+    if (newSizeStr !== null) {
+        const newSize = parseInt(newSizeStr);
+
+        if (!isNaN(newSize) && newSize > 0 && newSize <= 100) {
+            gridSize = newSize;
+            totalSquares = gridSize * gridSize;
+            createGrid(); 
+            const squares = document.querySelectorAll('.grid-square');
+            squares.forEach(square => {
+                square.addEventListener('mouseover', () => {
+                    square.style.backgroundColor = 'red';
+                });
+            });
+        } else {
+            alert("Invalid input. Please enter a number between 1 and 100.");
+        }
+    }
 });
